e2cf3998757177a05bb94c7f6b9b8454e38b634b
matiasblousson <mblousson@udesa.edu.ar>
2025-09-18 16:59:48 -0300

    Solucion errores y cambios en rutas

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const pc = require('../controllers/mercadoControllers');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', pc.showListadoProductos);
 
 module.exports = router;
