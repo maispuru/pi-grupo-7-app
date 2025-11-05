9f9874b5531c7b84001a3664b7bc464d1acff2c8
rvillafane1 <rvillafane@udesa.edu.ar>
2025-11-05 14:41:04 -0300

    config

---
+++ myapp/database/models/config/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "development": {
+    "username": "root",
+
+    "password": "",
+
+    "database": "proyectoIntegrador",
+
+    "host": "127.0.0.1",
+    
+    "dialect": "mysql"
+  },
+}
