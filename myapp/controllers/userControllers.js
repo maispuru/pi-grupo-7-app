1fc25a8267003ca90608eb8938856ba8d42acd6e
rvillafane1 <rvillafane@udesa.edu.ar>
2025-11-07 18:17:46 -0300

    login

--- myapp/controllers/userControllers.js
+++ myapp/controllers/userControllers.js
@@ -3,68 +3,78 @@ const bcryptjs = require('bcryptjs');
 const op = db.sequelize.Op ;
 
 const userControllers = {
-   show: function (req, res) {
+
+    show: function (req, res) {
     if (req.session.user != undefined) {
-      return res.redirect('/users/profile');
-    } else {
-      return res.render("register", { error: {} });
+      return res.redirect('/users/profile'); 
     }
+    return res.render('register', { error: {} });
   },
 
+
   create: function (req, res) {
+
     let usuario = req.body.usuario;
     let email = req.body.email;
     let contrasena = req.body.contrasena;
     let error = {};
 
-    if (email == "") {
-      error.email = "Email obligatorio";
-      return res.render("register", { error });
+    if (usuario === '') {
+      error.usuario = 'Nombre de usuario obligatorio';
+      return res.render('register', { error });
+    }
+    if (email === '') {
+      error.email = 'Email obligatorio';
+      return res.render('register', { error });
+    }
+    if (contrasena === '' || contrasena.length < 3) {
+      error.contrasena = 'La contraseña debe tener al menos 3 caracteres';
+      return res.render('register', { error });
     }
 
-    if (usuario == "") {
-      error.usuario = "Nombre de usuario obligatorio";
-      return res.render("register", { error });
-    }
-
-    if (contrasena == "" || contrasena.length < 3) {
-      error.contrasena = "La contraseña debe tener al menos 3 caracteres";
-      return res.render("register", { error });
-    }
-
-    let contrasenaEncriptada = bcryptjs.hashSync(contrasena, 10);
 
     db.Usuario.findOne({ where: { email: email } })
       .then(function (user) {
-        if (user != undefined) {
-          return res.send("El email ya existe");
-        } else {
-          let nuevoUsuario = {
-            usuario: usuario,
-            email: email,
-            contrasena: contrasenaEncriptada, 
-            createdAt: new Date()
-          };
 
-          db.Usuario.create(nuevoUsuario)
-            .then(function () {
-              return res.redirect("/users/login");
-            })
-            .catch(function (error) {
-              console.log(error);
-              return res.send(error);
-            });
+        if (user != undefined) {
+
+          return res.send("El email ya existe")
         }
-      });
+
+        db.Usuario.findOne({ where: { usuario: usuario } })
+
+          .then(function (usuario_1) {
+            if (usuario_1 != undefined) {
+              return res.send("El usuario ya existe")
+            }
+          })
+
+
+        let nuevoUsuario = {
+          usuario: usuario,
+          email: email,
+          contrasena: bcryptjs.hashSync(contrasena, 10),
+          createdAt: new Date()
+        };
+        db.Usuario.create(nuevoUsuario)
+          .then(function () {
+            return res.redirect("/users/login");
+          })
+          .catch(function (error) {
+            console.log(error);
+            return res.send(error);
+          });
+      }
+      );
   },
 
-  
-   showLogin: function (req, res) {
+  showLogin: function (req, res) {
     return res.render("login");
   },
-
-   createLogin: function (req, res) {
+  
+  createLogin: function (req, res) {
     let userInfo = {
+      id: req.body.id,
       email: req.body.email,
       contrasena: req.body.contrasena,
       recordarme: req.body.recordarme
@@ -73,12 +83,12 @@ const userControllers = {
     db.Usuario.findOne({ where: { email: userInfo.email } })
       .then(function (user) {
         if (user == undefined) {
-          return res.send("Este email no existe");
+          return res.send("este email no existe");
         }
 
         let validacion = bcryptjs.compareSync(userInfo.contrasena, user.contrasena);
         if (!validacion) {
-          return res.send("Contraseña incorrecta");
+          return res.send("contrasena incorrecta");
         }
 
         req.session.user = {
@@ -86,16 +96,15 @@ const userControllers = {
           email: user.email,
           usuario: user.usuario
         };
-
         if (userInfo.recordarme != undefined) {
-          res.cookie("user", req.session.user, { maxAge: 150000 });
+          res.cookie("user", user, { maxAge: 150000 })
         }
-          return res.redirect("/users/profile");
-      })
 
+        return res.redirect("/users/profile");
+      })
       .catch(function (error) {
         console.log(error);
-        return res.send("Error en login");
+        return res.send("error en login");
       });
   },
 
